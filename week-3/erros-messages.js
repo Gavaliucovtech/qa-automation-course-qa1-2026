7ee9ef9bdc31fa967cd33196409d6cee4cd06f26
Victoria <victoria.axentii93@gmail.com>
2026-09-05 15:58:46 -0400

    Error messages

---
+++ week-3/erros-messages.js
@@ -0,0 +1,17 @@
+// SYNTAX ERRORS
+
+// let girlName = "Alice
+// console.log(name);
+
+// REFERENCE ERRORS
+
+//console.log(age);
+
+// TYPE ERROS
+
+// let num = 5;
+// console.log(num.toUpperCase());
+
+// RANGE ERRORS
+
+//console.log("hi".repeat(-5));
