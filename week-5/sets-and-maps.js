00e7b6942f469a814842a0b1fb91c0b20c2aa74e
Victoria <victoria.axentii93@gmail.com>
2026-09-16 19:51:10 -0400

    Sets and Maps

---
+++ week-5/sets-and-maps.js
@@ -0,0 +1,88 @@
+const numbers = new Set([1, 2, 3, 3, 4]);
+console.log(numbers);
+
+const mySet = new Set();
+
+// Add values
+
+mySet.add("apple");
+mySet.add("banana");
+mySet.add("apple");
+
+console.log(mySet);
+
+//Check if a value exists
+
+console.log(mySet.has("apple"));
+console.log(mySet.has("grape"));
+
+// delete a value
+
+mySet.delete("banana");
+console.log(mySet);
+
+// Size of Set
+
+console.log(mySet.size);
+
+// Iterating throught a set
+
+const fruits = new Set(["apple", "banana", "cherry"]);
+for (const fruit of fruits) {
+  console.log(fruit);
+}
+
+// Use case: Removing Dublicates from an array
+// Maybe a interview question !!!!! How to remove dublicates????/
+
+const numberss = [1, 2, 2, 3, 4, 4, 5];
+
+const uniqueNumbers = [...new Set(numberss)];
+console.log(uniqueNumbers);
+
+// MAPS
+
+const mMap = new Map();
+const initialMap = new Map([
+  ["name", "Alice"],
+  ["age", 30],
+]);
+const mySecondMap = new Map();
+
+mySecondMap.set("name", "Alice");
+mySecondMap.set("age", 25);
+mySecondMap.set(true, "This is a boolean key");
+
+console.log(mySecondMap);
+
+const userMap = new Map();
+
+// Add entries
+userMap.set("username", "Bob");
+userMap.set("role", "admin");
+
+// Retrive value
+console.log(userMap.get("username"));
+
+//check if a key exists
+
+console.log(userMap.has("role"));
+
+// delete a key
+
+userMap.delete("role");
+console.log(userMap.size);
+
+// Iterating throught a MAP
+
+const studentMap = new Map([
+  ["name", "Alice"],
+  ["age", 22],
+  ["grade", "A"],
+]);
+
+// Using for...of
+
+for (const [key, value] of studentMap) {
+  console.log(`${key}; ${value}`);
+}
