066e08835a6f7a0148f663d97527459bdce935d6
Victoria <victoria.axentii93@gmail.com>
2026-09-26 14:04:25 -0400

    Asynchronous JavaScript

--- week-7/async.js
+++ week-7/async.js
@@ -15,3 +15,34 @@ setTimeout(() => {
 console.log("End");
 
 // Create a promise
+
+// Maker: creates the promise
+
+function makeBurger() {
+  return new Promise((resolve, reject) => {
+    console.log("Making your burger...");
+
+    setTimeout(() => {
+      const success = true; //simulate result
+      if (success) {
+        console.log("Burget ready!");
+        resolve("Burger served!");
+      } else {
+        reject("Burger burnt");
+      }
+    }, 3000);
+  });
+}
+
+// Receiver: waits for the promise
+
+async function orderMeal() {
+  try {
+    const result = await makeBurger(); // receiver waits here
+    console.log("Receiver:", result);
+    console.log("Enjoy your meal!");
+  } catch (error) {
+    console.log("Receiver error:", error);
+  }
+}
+await orderMeal();
